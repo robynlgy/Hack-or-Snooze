b80e5043da25c8e600ca8fd92809fd3365ad87cc
Robyn Lam <robynlamgy@gmail.com>
2022-03-17 12:18:47 -0700

    WIP subpart 2B

---
+++ js/nav.js
@@ -0,0 +1,48 @@
+"use strict";
+
+/******************************************************************************
+ * Handling navbar clicks and updating navbar
+ */
+
+/** Show main list of all stories when click site name */
+
+function navAllStories(evt) {
+  console.debug("navAllStories", evt);
+  evt.preventDefault();
+  hidePageComponents();
+  putStoriesOnPage();
+}
+
+$body.on("click", "#nav-all", navAllStories);
+
+/** Show login/signup on click on "login" */
+
+function navLoginClick(evt) {
+  console.debug("navLoginClick", evt);
+  evt.preventDefault();
+  hidePageComponents();
+  $loginForm.show();
+  $signupForm.show();
+}
+
+$navLogin.on("click", navLoginClick);
+
+/** When a user first logins in, update the navbar to reflect that. */
+
+function updateNavOnLogin() {
+  console.debug("updateNavOnLogin");
+  $(".main-nav-links").show();
+  $navLogin.hide();
+  $navLogOut.show();
+  $navUserProfile.text(`${currentUser.username}`).show();
+}
+
+/** When a user click submit in the nav bar, update stories list with new story. */
+
+function navSubmitClick() {
+  $submitForm.show();
+}
+
+$navSubmit.on("click", navSubmitClick);
+
+
